1d9c6c233b60c9fd33608f4b59b8b60d9bf97f82
pavel <shirokuiuna@mail.ru>
2023-09-27 20:08:59 +0300

    Главная страница + фикс стилей

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,26 @@
 function MainPage() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__logo logo">Cinemaddict</h1>
+
+        {/* Звание пользователя */}
+      </header>
+
+      <main className="main">
+        {/* Меню */}
+
+        {/* Сортировка */}
+
+        {/* Контент */}
+      </main>
+
+      <footer className="footer">
+        <section className="footer__logo logo logo--smaller">Cinemaddict</section>
+        <section className="footer__statistics">{/* Количество фильмов */}</section>
+      </footer>
+    </>
+  );
 }
 
 export default MainPage;
