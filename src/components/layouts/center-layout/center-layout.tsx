9629b8cd07e7bcd386b73f73eaca0ba567fb9015
pavel <shirokuiuna@mail.ru>
2023-09-27 19:55:39 +0300

    Удалил ненужные файл + подключил стили

--- src/components/layouts/center-layout/center-layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-
-import './center-layout.scss';
-
-function CenterLayout({ children }: PropsWithChildren<any>) {
-  return <div className="center-layout">{children}</div>;
-}
-
-export default CenterLayout;
